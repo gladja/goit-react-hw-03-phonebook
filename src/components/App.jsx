3fe9484d6b500a31d066ca7de7f8b33eb60887e7
gladja <gladja@yandex.ru>
2023-09-17 10:12:23 +0300

    add localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,6 +15,22 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+    this.setState({ contacts: parsedContacts})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    if(this.state.contacts !== prevState.contacts) {
+      console.log('yes');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   handleSubmit = (e) => {
     e.preventDefault();
 
